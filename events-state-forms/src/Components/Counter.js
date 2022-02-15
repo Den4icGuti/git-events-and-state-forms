ed3cdb03d364bdb929fdfb6290682195a8b0dd5f
Den4icGuti <goos7412@gmail.com>
2022-02-15 08:47:24 +0200

    add button element

--- events-state-forms/src/Components/Counter.js
+++ events-state-forms/src/Components/Counter.js
@@ -4,33 +4,35 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  /* ... */
+
+  handleIncrement = evt => {
+    console.log("Increment button was clicked!", evt); // работает
+    console.log("this.props: ", this.props); // работает
+  
+  };
+
+  handleDecrement = evt => {
+    console.log("Decrement button was clicked!", evt); // работает
+    console.log("this.props: ", this.props); // работает
+  };
+
   render() {
     const { step } = this.props;
 
     return (
       <div>
-        <span>0</span>
-        <button
-          type="button"
-          onClick={evt => {
-            console.log("Increment button was clicked!", evt); // работает
-            console.log("this.props: ", this.props); // работает
-          }}
-        >
-           Increment by {step}
+        
+        <button type="button" onClick={this.handleIncrement}>
+         Increment by {step}
         </button>
-        <button
-          type="button"
-          onClick={evt => {
-            console.log("Decrement button was clicked!", evt); // работает
-            console.log("this.props: ", this.props); // работает
-          }}
-        >
+          <span>0</span>
+        <button type="button" onClick={this.handleDecrement}>
           Decrement by {step}
         </button>
       </div>
-    )
+    );
   }
 }
 
-export default Counter
+export default Counter;
